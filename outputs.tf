50e0ab3b68c00728ee0474d6c0e8515176fe4c25
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:02:59 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "security_center_storage_defenders" {
+  description = "All security_center_storage_defender resources"
+  value       = azurerm_security_center_storage_defender.security_center_storage_defenders
+}
+
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "security_center_storage_defenders" {
+  description = <<EOT
+Map of security_center_storage_defenders, attributes below
+Required:
+    - storage_account_id
+Optional:
+    - malware_scanning_on_upload_cap_gb_per_month
+    - malware_scanning_on_upload_enabled
+    - override_subscription_settings_enabled
+    - scan_results_event_grid_topic_id
+    - sensitive_data_discovery_enabled
+EOT
+
+  type = map(object({
+    storage_account_id                          = string
+    malware_scanning_on_upload_cap_gb_per_month = optional(number, -1)
+    malware_scanning_on_upload_enabled          = optional(bool, false)
+    override_subscription_settings_enabled      = optional(bool, false)
+    scan_results_event_grid_topic_id            = optional(string)
+    sensitive_data_discovery_enabled            = optional(bool, false)
+  }))
+}
+
