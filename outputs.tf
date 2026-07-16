9ccf4154e29ef9060eeec598bfd842c4d41e227e
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "security_center_storage_defenders_id" {
   description = "Map of id values across all security_center_storage_defenders, keyed the same as var.security_center_storage_defenders"
-  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.id }
+  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "security_center_storage_defenders_malware_scanning_on_upload_cap_gb_per_month" {
   description = "Map of malware_scanning_on_upload_cap_gb_per_month values across all security_center_storage_defenders, keyed the same as var.security_center_storage_defenders"
-  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.malware_scanning_on_upload_cap_gb_per_month }
+  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.malware_scanning_on_upload_cap_gb_per_month if v.malware_scanning_on_upload_cap_gb_per_month != null }
 }
 output "security_center_storage_defenders_malware_scanning_on_upload_enabled" {
   description = "Map of malware_scanning_on_upload_enabled values across all security_center_storage_defenders, keyed the same as var.security_center_storage_defenders"
-  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.malware_scanning_on_upload_enabled }
+  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.malware_scanning_on_upload_enabled if v.malware_scanning_on_upload_enabled != null }
 }
 output "security_center_storage_defenders_override_subscription_settings_enabled" {
   description = "Map of override_subscription_settings_enabled values across all security_center_storage_defenders, keyed the same as var.security_center_storage_defenders"
-  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.override_subscription_settings_enabled }
+  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.override_subscription_settings_enabled if v.override_subscription_settings_enabled != null }
 }
 output "security_center_storage_defenders_scan_results_event_grid_topic_id" {
   description = "Map of scan_results_event_grid_topic_id values across all security_center_storage_defenders, keyed the same as var.security_center_storage_defenders"
-  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.scan_results_event_grid_topic_id }
+  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.scan_results_event_grid_topic_id if v.scan_results_event_grid_topic_id != null && length(v.scan_results_event_grid_topic_id) > 0 }
 }
 output "security_center_storage_defenders_sensitive_data_discovery_enabled" {
   description = "Map of sensitive_data_discovery_enabled values across all security_center_storage_defenders, keyed the same as var.security_center_storage_defenders"
-  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.sensitive_data_discovery_enabled }
+  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.sensitive_data_discovery_enabled if v.sensitive_data_discovery_enabled != null }
 }
 output "security_center_storage_defenders_storage_account_id" {
   description = "Map of storage_account_id values across all security_center_storage_defenders, keyed the same as var.security_center_storage_defenders"
-  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.storage_account_id }
+  value       = { for k, v in azurerm_security_center_storage_defender.security_center_storage_defenders : k => v.storage_account_id if v.storage_account_id != null && length(v.storage_account_id) > 0 }
 }
 
